5e0d063acaca3778fceb595410b234d041915329
CvzFranco <franco.code@outlook.com>
2021-10-02 07:12:00 +0200

    Basic DOM manipulation

--- Section 6 - Guess My Number/index.js
+++ Section 6 - Guess My Number/index.js
@@ -1,3 +1,10 @@
 'use strict';
 
+document.querySelector('.message').textContent = 'Correct Number';
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textcontent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
