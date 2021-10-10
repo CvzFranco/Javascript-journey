53de0bb56e7a18dee5afcb996d58cced950223f1
CvzFranco <franco.code@outlook.com>
2021-10-10 10:16:30 +0200

    Remainder operator

--- Section 12 : Numbers-Dates-Times-Bankis/script.js
+++ Section 12 : Numbers-Dates-Times-Bankis/script.js
@@ -328,3 +328,31 @@ const randomInt = (min, max) =>
 // console.log((2.7).toFixed(0));
 // console.log((2.7).toFixed(3));
 // console.log(+(2.23467).toFixed(2));
+
+/**
+ * Remainder Oprator
+ */
+
+console.log(5 % 2);
+console.log(5 / 2); // 5 = 2 * 2 + 1
+console.log(8 % 3); // 8 = 3 * 2 + 2
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+labelBalance.addEventListener('click', function (e) {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    //0,2,4,6...
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    //0 ,3,6,9...
+    if (i % 3 === 0) row.style.backgroundColor = 'blue';
+  });
+});
