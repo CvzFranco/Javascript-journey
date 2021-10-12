62b894c71fb6345f03bdc19369d8327629db68c0
CvzFranco <franco.code@outlook.com>
2021-10-12 21:34:29 +0200

    class , heritages, prototype

---
+++ Js / NODEJS/1-creation_d_un_modele_d_objet_en_javascript/assets/js/main.js
@@ -0,0 +1,105 @@
+"use strict";
+
+// //Objet Vide
+// const paul = {}; // = new Object()
+// const alice = {};
+
+//MODEL
+/*
+function Person(firstName, lastName, age) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.age = age;
+  //Method
+  this.hello = function () {
+    console.log(`Hello ${firstName} ${lastName}`);
+  };
+}
+*/
+
+// franco.hello(); // Hello Franco Chiavuzzo
+
+//Heritage function
+
+// function Etudiant (firstname, lastname, age, level, sector){
+//     this.person = Person;
+//     this.person(firstname,lastname,age)
+//     this.age = age
+//     this.sector = sector
+
+//     this.getLevel = function(){
+//         console.log(`Level : ${level}`);
+//     }
+//     this.getSector = function(){
+//         console.log(`Sector : ${sector}`);
+//     }
+// }
+
+//Heritage Class
+// Class ECMAScript 2015
+class Person {
+  constructor(firstName, lastName, age) {
+    this.lastName = lastName;
+    this.firstName = firstName;
+    this.age = age;
+    this.createdAt = new Date();
+  }
+  hello() {
+    console.log(`Hello ${this.firstName} ${this.lastName}`);
+  }
+  getAge() {
+    console.log(
+      `Hello. je m'appelle ${this.firstName} ${this.lastName} et j'ai ${this.age}`
+    );
+  }
+}
+
+// Model Etudiant qui Hérite du model Person
+class Etudiant extends Person {
+  constructor(firstName, lastName, age, sector, level) {
+    super(firstName, lastName, age); // Fait reference a la clase dont on herite
+    this.level = level;
+    this.sector = sector;
+  }
+  getLevel() {
+    console.log(`Level : ${this.level}`);
+  }
+  getSector() {
+    console.log(`Secteur : ${this.sector}`);
+  }
+}
+
+const getHeritages = (obj) => {
+  let prototype = Object.getPrototypeOf(obj);
+
+  const result = [];
+
+  while (prototype) {
+    result[result.length] = prototype.constructor.name;
+    prototype = Object.getPrototypeOf(prototype);
+  }
+  return result;
+};
+
+const franco = new Person("Franco", "Chiavuzzo", 32);
+const sophie = new Person("Sophie", "Mattauer", 32);
+const diane = new Etudiant(
+  "Diane",
+  "Chiavuzzo",
+  4,
+  "Escalade",
+  "Moyenne Section"
+);
+
+// console.log(diane);
+// diane.getLevel();
+
+//Protype
+
+// console.log(typeof diane);
+// console.log(typeof franco);
+
+//Connaitre le prototype , modele à partir duquel franco à été crée et celui dont il a hérité.
+// console.log(Object.getPrototypeOf(diane));
+console.log(getHeritages(diane));
+console.log(getHeritages(franco));
