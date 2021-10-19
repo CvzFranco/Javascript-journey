0f145d50c8702031f560cd8c41b921036a14f9e2
CvzFranco <franco.code@outlook.com>
2021-10-19 04:45:05 +0200

    promise

--- Section 16 : Async/script.js
+++ Section 16 : Async/script.js
@@ -5,34 +5,96 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-//most old school one
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v2/name/${country}`);
-  request.send();
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class="country ${className}" >
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article
+  `;
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    const html = `
-  <article class="country">
-  <img class="country__img" src="${data.flag}" />
-  <div class="country__data">
-    <h3 class="country__name">${data.name}</h3>
-    <h4 class="country__region">${data.region}</h4>
-    <p class="country__row"><span>👫</span>${(
-      +data.population / 1000000
-    ).toFixed(1)}</p>
-    <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-    <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-  </div>
-</article
-`;
-
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  });
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 };
-getCountryData('france');
-getCountryData('italia');
-getCountryData('spain');
+
+// //most old school one
+// const getCountryDataAndNeighbour = function (country) {
+//   //AJAX country 1
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     //Render Cuountry 1
+//     renderCountry(data);
+
+//     //GET neightbour country (2)
+//     const [neighbour] = data.borders;
+
+//     if (!neighbour) return;
+
+//     //AJAX country 2
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+//     request2.send();
+
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+
+//       renderCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+
+// // getCountryDataAndNeighbour('portugal');
+// getCountryDataAndNeighbour('usa');
+
+// //Call Back HELL ,Hard to understand
+// setTimeout(() => {
+//   console.log('1second passes');
+//   setTimeout(() => {
+//     console.log('2second passes');
+//     setTimeout(() => {
+//       console.log('3second passes');
+//       setTimeout(() => {
+//         console.log('4second passes');
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+// const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v2/name/${country}`);
+//   request.send();
+
+//promise
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       // fullField promise
+//       console.log(response);
+//       return response.json(); //it's a new promise , tjr a faire sur la reponse afin de pouvoir la lire.
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => renderCountry(data[0]));
+};
+getCountryData('portugal');
