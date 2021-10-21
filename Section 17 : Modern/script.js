a314c7e27afe265b9fd3dc29b2d3e8c644abc81e
CvzFranco <franco.code@outlook.com>
2021-10-21 20:05:15 +0200

    End of session

---
+++ Section 17 : Modern/script.js
@@ -0,0 +1,47 @@
+//Importing module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq);
+// console.log('Importing module');
+
+// import shoppingCart from './shoppingCart';
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+// console.log(price);
+
+// import add, { cart } from './shoppingCart.js';
+// add('pizza', 2);
+// add('break', 2);
+// add('apples', 2);
+
+// console.log(cart);
+
+// const ShoppingCart2 = (function () {
+//   const cart = [];
+//   const shippingCost = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} added to cart`);
+//   };
+
+//   const orderStock = function (product, quantity) {
+//     console.log(`${quantity} ${product} ordered from supplier `);
+//   };
+
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
+
+// ShoppingCart2.addToCart('apple', 4);
+// ShoppingCart2.addToCart('pizza', 2);
+// console.log(ShoppingCart2);
+// console.log(ShoppingCart2.shippingCost);
